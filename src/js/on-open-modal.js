385e7df9dd95c34748429b3d9a2e85c3b66ee9af
Sergey-Proskurnin <proskurnin@ua.fm>
2021-04-29 01:08:02 +0300

    Adds: options on basicLightbox for fixate body when open modal and alt for img- modal

--- src/js/on-open-modal.js
+++ src/js/on-open-modal.js
@@ -4,10 +4,16 @@ export default function onOpenModal(event) {
   let escBtn = '';
   if (event.target.localName === 'img') {
     const imgListSrc = event.target.getAttribute('data-src');
+    const altImg = event.target.getAttribute('alt');
     const instance = basicLightbox.create(
-      `<img src=${imgListSrc} width="1280">`,
+      `<img src=${imgListSrc} alt="${altImg}" width="1280">`,
+      {
+        onClose: () => {
+          document.body.classList.remove('is-open');
+        },
+      },
     );
-    instance.show();
+    instance.show(document.body.classList.add('is-open'));
     escBtn = window.addEventListener('keyup', event => {
       if (event.key === 'Escape') {
         instance.close();
